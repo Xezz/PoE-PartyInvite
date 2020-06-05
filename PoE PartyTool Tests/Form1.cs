2a007dc992fe3fef14aeafbe879cefe58f854d47
Xezz <xezzxezz@gmail.com>
2020-06-05 15:59:35 +0200

    Fixes #1 #2
    Tests hinzugefügt und Project und Solution umbennant

--- PoE PartyTool Tests/Form1.cs
+++ PoE PartyTool Tests/Form1.cs
@@ -14,6 +14,7 @@ namespace PoE_PartyTool_Tests
 {
 	public partial class Form1 : Form
 	{
+		// Todo Dependency Injection
 		PoEProcessWatcher processWatcher = new PoEProcessWatcher();
 		LogFileReader logReader = new LogFileReader();
 		LogFileParser logParser = new LogFileParser();
