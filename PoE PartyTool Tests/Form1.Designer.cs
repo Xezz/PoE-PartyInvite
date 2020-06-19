faa5e66bf2a662f20e14813b0ad556595a003a3e
Krytech_PC2 <Krytech5013@gmail.com>
2020-06-19 15:25:46 +0200

    Created Settings Dialog

--- PoE PartyTool Tests/Form1.Designer.cs
+++ PoE PartyTool Tests/Form1.Designer.cs
@@ -28,8 +28,6 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.slider_Opacity = new System.Windows.Forms.TrackBar();
-			this.label4 = new System.Windows.Forms.Label();
 			this.btn_left = new System.Windows.Forms.Button();
 			this.btn_right = new System.Windows.Forms.Button();
 			this.btn_inv = new System.Windows.Forms.Button();
@@ -37,39 +35,21 @@
 			this.lbl_requestCount = new System.Windows.Forms.Label();
 			this.lbl_currentNr = new System.Windows.Forms.Label();
 			this.lbl_currentName = new System.Windows.Forms.Label();
-			this.label1 = new System.Windows.Forms.Label();
-			this.label2 = new System.Windows.Forms.Label();
+			this.lbl_Pending = new System.Windows.Forms.Label();
+			this.lbl_Selected = new System.Windows.Forms.Label();
 			this.cb_XezzMode = new System.Windows.Forms.CheckBox();
-			((System.ComponentModel.ISupportInitialize)(this.slider_Opacity)).BeginInit();
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.menuStrip1.SuspendLayout();
 			this.SuspendLayout();
-			// 
-			// slider_Opacity
-			// 
-			this.slider_Opacity.Location = new System.Drawing.Point(0, 10);
-			this.slider_Opacity.Maximum = 100;
-			this.slider_Opacity.Minimum = 25;
-			this.slider_Opacity.Name = "slider_Opacity";
-			this.slider_Opacity.Size = new System.Drawing.Size(84, 45);
-			this.slider_Opacity.TabIndex = 11;
-			this.slider_Opacity.Value = 100;
-			this.slider_Opacity.Scroll += new System.EventHandler(this.trackBar1_Scroll);
-			// 
-			// label4
-			// 
-			this.label4.AutoSize = true;
-			this.label4.Location = new System.Drawing.Point(17, 2);
-			this.label4.Name = "label4";
-			this.label4.Size = new System.Drawing.Size(43, 13);
-			this.label4.TabIndex = 12;
-			this.label4.Text = "Opacity";
 			// 
 			// btn_left
 			// 
 			this.btn_left.Enabled = false;
-			this.btn_left.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.btn_left.Location = new System.Drawing.Point(83, 2);
+			this.btn_left.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btn_left.Location = new System.Drawing.Point(87, 25);
 			this.btn_left.Name = "btn_left";
-			this.btn_left.Size = new System.Drawing.Size(33, 23);
+			this.btn_left.Size = new System.Drawing.Size(30, 23);
 			this.btn_left.TabIndex = 13;
 			this.btn_left.Text = "<-";
 			this.btn_left.UseVisualStyleBackColor = true;
@@ -78,10 +58,10 @@
 			// btn_right
 			// 
 			this.btn_right.Enabled = false;
-			this.btn_right.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.btn_right.Location = new System.Drawing.Point(125, 2);
+			this.btn_right.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btn_right.Location = new System.Drawing.Point(125, 25);
 			this.btn_right.Name = "btn_right";
-			this.btn_right.Size = new System.Drawing.Size(33, 23);
+			this.btn_right.Size = new System.Drawing.Size(30, 23);
 			this.btn_right.TabIndex = 14;
 			this.btn_right.Text = "->";
 			this.btn_right.UseVisualStyleBackColor = true;
@@ -90,9 +70,10 @@
 			// btn_inv
 			// 
 			this.btn_inv.Enabled = false;
-			this.btn_inv.Location = new System.Drawing.Point(83, 31);
+			this.btn_inv.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btn_inv.Location = new System.Drawing.Point(87, 52);
 			this.btn_inv.Name = "btn_inv";
-			this.btn_inv.Size = new System.Drawing.Size(75, 23);
+			this.btn_inv.Size = new System.Drawing.Size(50, 23);
 			this.btn_inv.TabIndex = 15;
 			this.btn_inv.Text = "inv";
 			this.btn_inv.UseVisualStyleBackColor = true;
@@ -101,7 +82,8 @@
 			// btn_remove
 			// 
 			this.btn_remove.Enabled = false;
-			this.btn_remove.Location = new System.Drawing.Point(83, 57);
+			this.btn_remove.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btn_remove.Location = new System.Drawing.Point(145, 52);
 			this.btn_remove.Name = "btn_remove";
 			this.btn_remove.Size = new System.Drawing.Size(75, 23);
 			this.btn_remove.TabIndex = 16;
@@ -112,7 +94,7 @@
 			// lbl_requestCount
 			// 
 			this.lbl_requestCount.AutoSize = true;
-			this.lbl_requestCount.Location = new System.Drawing.Point(57, 45);
+			this.lbl_requestCount.Location = new System.Drawing.Point(71, 30);
 			this.lbl_requestCount.Name = "lbl_requestCount";
 			this.lbl_requestCount.Size = new System.Drawing.Size(13, 13);
 			this.lbl_requestCount.TabIndex = 17;
@@ -121,7 +103,7 @@
 			// lbl_currentNr
 			// 
 			this.lbl_currentNr.AutoSize = true;
-			this.lbl_currentNr.Location = new System.Drawing.Point(57, 67);
+			this.lbl_currentNr.Location = new System.Drawing.Point(71, 57);
 			this.lbl_currentNr.Name = "lbl_currentNr";
 			this.lbl_currentNr.Size = new System.Drawing.Size(13, 13);
 			this.lbl_currentNr.TabIndex = 18;
@@ -130,48 +112,65 @@
 			// lbl_currentName
 			// 
 			this.lbl_currentName.AutoSize = true;
-			this.lbl_currentName.Location = new System.Drawing.Point(9, 89);
+			this.lbl_currentName.Location = new System.Drawing.Point(10, 80);
 			this.lbl_currentName.Name = "lbl_currentName";
 			this.lbl_currentName.Size = new System.Drawing.Size(33, 13);
 			this.lbl_currentName.TabIndex = 19;
 			this.lbl_currentName.Text = "None";
 			// 
-			// label1
+			// lbl_Pending
 			// 
-			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(-2, 45);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(49, 13);
-			this.label1.TabIndex = 20;
-			this.label1.Text = "Pending:";
+			this.lbl_Pending.AutoSize = true;
+			this.lbl_Pending.Location = new System.Drawing.Point(12, 30);
+			this.lbl_Pending.Name = "lbl_Pending";
+			this.lbl_Pending.Size = new System.Drawing.Size(17, 13);
+			this.lbl_Pending.TabIndex = 20;
+			this.lbl_Pending.Text = "Pending:";
 			// 
-			// label2
+			// lbl_Selected
 			// 
-			this.label2.AutoSize = true;
-			this.label2.Location = new System.Drawing.Point(-2, 67);
-			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(52, 13);
-			this.label2.TabIndex = 21;
-			this.label2.Text = "Selected:";
+			this.lbl_Selected.AutoSize = true;
+			this.lbl_Selected.Location = new System.Drawing.Point(12, 57);
+			this.lbl_Selected.Name = "lbl_Selected";
+			this.lbl_Selected.Size = new System.Drawing.Size(17, 13);
+			this.lbl_Selected.TabIndex = 21;
+			this.lbl_Selected.Text = "Selected:";
 			// 
 			// cb_XezzMode
 			// 
 			this.cb_XezzMode.AutoSize = true;
-			this.cb_XezzMode.Location = new System.Drawing.Point(168, 6);
+			this.cb_XezzMode.Location = new System.Drawing.Point(164, 31);
 			this.cb_XezzMode.Name = "cb_XezzMode";
-			this.cb_XezzMode.Size = new System.Drawing.Size(76, 17);
+			this.cb_XezzMode.Size = new System.Drawing.Size(42, 17);
 			this.cb_XezzMode.TabIndex = 22;
 			this.cb_XezzMode.Text = "XezzMode";
 			this.cb_XezzMode.UseVisualStyleBackColor = true;
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.settingsToolStripMenuItem});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(165, 24);
+			this.menuStrip1.TabIndex = 23;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// settingsToolStripMenuItem
+			// 
+			this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+			this.settingsToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+			this.settingsToolStripMenuItem.Text = "Settings";
+			this.settingsToolStripMenuItem.Click += new System.EventHandler(this.settingsToolStripMenuItem_Click);
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(264, 113);
+			this.ClientSize = new System.Drawing.Size(264, 105);
 			this.Controls.Add(this.cb_XezzMode);
-			this.Controls.Add(this.label2);
-			this.Controls.Add(this.label1);
+			this.Controls.Add(this.lbl_Selected);
+			this.Controls.Add(this.lbl_Pending);
 			this.Controls.Add(this.lbl_currentName);
 			this.Controls.Add(this.lbl_currentNr);
 			this.Controls.Add(this.lbl_requestCount);
@@ -179,29 +178,30 @@
 			this.Controls.Add(this.btn_inv);
 			this.Controls.Add(this.btn_right);
 			this.Controls.Add(this.btn_left);
-			this.Controls.Add(this.label4);
-			this.Controls.Add(this.slider_Opacity);
+			this.Controls.Add(this.menuStrip1);
+			this.MainMenuStrip = this.menuStrip1;
 			this.Name = "Form1";
 			this.Text = "Form1";
-			((System.ComponentModel.ISupportInitialize)(this.slider_Opacity)).EndInit();
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
 		}
 
 		#endregion
-		private System.Windows.Forms.TrackBar slider_Opacity;
-		private System.Windows.Forms.Label label4;
-		private System.Windows.Forms.Button btn_left;
-		private System.Windows.Forms.Button btn_right;
-		private System.Windows.Forms.Button btn_inv;
-		private System.Windows.Forms.Button btn_remove;
-		private System.Windows.Forms.Label lbl_requestCount;
-		private System.Windows.Forms.Label lbl_currentNr;
-		private System.Windows.Forms.Label lbl_currentName;
-		private System.Windows.Forms.Label label1;
-		private System.Windows.Forms.Label label2;
-		private System.Windows.Forms.CheckBox cb_XezzMode;
+		public System.Windows.Forms.Button btn_left;
+		public System.Windows.Forms.Button btn_right;
+		public System.Windows.Forms.Button btn_inv;
+		public System.Windows.Forms.Button btn_remove;
+		public System.Windows.Forms.Label lbl_requestCount;
+		public System.Windows.Forms.Label lbl_currentNr;
+		public System.Windows.Forms.Label lbl_currentName;
+		public System.Windows.Forms.Label lbl_Pending;
+		public System.Windows.Forms.Label lbl_Selected;
+		public System.Windows.Forms.CheckBox cb_XezzMode;
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
 	}
 }
 
---
+++ PoE PartyTool Tests/SettingsDialog.cs
@@ -0,0 +1,309 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PoE_PartyTool_Tests
+{
+	public partial class SettingsDialog : Form
+	{
+		Form1 mainDialogWindow;
+
+		public SettingsDialog(Form1 main)
+		{
+			InitializeComponent();
+
+			mainDialogWindow = main;
+		}
+
+		private void slider_Opacity_Scroll(object sender, EventArgs e)
+		{
+			mainDialogWindow.Opacity = Convert.ToDouble(slider_Opacity.Value) / 100;
+		}
+
+		private void btn_Layout1_Click(object sender, EventArgs e)
+		{
+			mainDialogWindow.btn_left.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			mainDialogWindow.btn_left.Location = new System.Drawing.Point(87, 25);
+			mainDialogWindow.btn_left.Name = "btn_left";
+			mainDialogWindow.btn_left.Size = new System.Drawing.Size(30, 23);
+			mainDialogWindow.btn_left.TabIndex = 13;
+			mainDialogWindow.btn_left.Text = "<-";
+			mainDialogWindow.btn_left.UseVisualStyleBackColor = true;
+
+			mainDialogWindow.btn_right.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			mainDialogWindow.btn_right.Location = new System.Drawing.Point(125, 25);
+			mainDialogWindow.btn_right.Name = "btn_right";
+			mainDialogWindow.btn_right.Size = new System.Drawing.Size(30, 23);
+			mainDialogWindow.btn_right.TabIndex = 14;
+			mainDialogWindow.btn_right.Text = "->";
+			mainDialogWindow.btn_right.UseVisualStyleBackColor = true;
+
+			mainDialogWindow.btn_inv.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			mainDialogWindow.btn_inv.Location = new System.Drawing.Point(87, 52);
+			mainDialogWindow.btn_inv.Name = "btn_inv";
+			mainDialogWindow.btn_inv.Size = new System.Drawing.Size(50, 23);
+			mainDialogWindow.btn_inv.TabIndex = 15;
+			mainDialogWindow.btn_inv.Text = "inv";
+			mainDialogWindow.btn_inv.UseVisualStyleBackColor = true;
+
+			mainDialogWindow.btn_remove.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			mainDialogWindow.btn_remove.Location = new System.Drawing.Point(145, 52);
+			mainDialogWindow.btn_remove.Name = "btn_remove";
+			mainDialogWindow.btn_remove.Size = new System.Drawing.Size(75, 23);
+			mainDialogWindow.btn_remove.TabIndex = 16;
+			mainDialogWindow.btn_remove.Text = "remove";
+			mainDialogWindow.btn_remove.UseVisualStyleBackColor = true;
+
+			mainDialogWindow.lbl_requestCount.AutoSize = true;
+			mainDialogWindow.lbl_requestCount.Location = new System.Drawing.Point(71, 30);
+			mainDialogWindow.lbl_requestCount.Name = "lbl_requestCount";
+			mainDialogWindow.lbl_requestCount.Size = new System.Drawing.Size(13, 13);
+			mainDialogWindow.lbl_requestCount.TabIndex = 17;
+			mainDialogWindow.lbl_requestCount.Text = "0";
+
+			mainDialogWindow.lbl_currentNr.AutoSize = true;
+			mainDialogWindow.lbl_currentNr.Location = new System.Drawing.Point(71, 57);
+			mainDialogWindow.lbl_currentNr.Name = "lbl_currentNr";
+			mainDialogWindow.lbl_currentNr.Size = new System.Drawing.Size(13, 13);
+			mainDialogWindow.lbl_currentNr.TabIndex = 18;
+			mainDialogWindow.lbl_currentNr.Text = "0";
+
+			mainDialogWindow.lbl_currentName.AutoSize = true;
+			mainDialogWindow.lbl_currentName.Location = new System.Drawing.Point(10, 80);
+			mainDialogWindow.lbl_currentName.Name = "lbl_currentName";
+			mainDialogWindow.lbl_currentName.Size = new System.Drawing.Size(33, 13);
+			mainDialogWindow.lbl_currentName.TabIndex = 19;
+			mainDialogWindow.lbl_currentName.Text = "None";
+
+			mainDialogWindow.lbl_Pending.AutoSize = true;
+			mainDialogWindow.lbl_Pending.Location = new System.Drawing.Point(12, 30);
+			mainDialogWindow.lbl_Pending.Name = "lbl_Pending";
+			mainDialogWindow.lbl_Pending.Size = new System.Drawing.Size(17, 13);
+			mainDialogWindow.lbl_Pending.TabIndex = 20;
+			mainDialogWindow.lbl_Pending.Text = "Pending:";
+
+			mainDialogWindow.lbl_Selected.AutoSize = true;
+			mainDialogWindow.lbl_Selected.Location = new System.Drawing.Point(12, 57);
+			mainDialogWindow.lbl_Selected.Name = "lbl_Selected";
+			mainDialogWindow.lbl_Selected.Size = new System.Drawing.Size(17, 13);
+			mainDialogWindow.lbl_Selected.TabIndex = 21;
+			mainDialogWindow.lbl_Selected.Text = "Selected:";
+
+			mainDialogWindow.cb_XezzMode.AutoSize = true;
+			mainDialogWindow.cb_XezzMode.Location = new System.Drawing.Point(164, 31);
+			mainDialogWindow.cb_XezzMode.Name = "cb_XezzMode";
+			mainDialogWindow.cb_XezzMode.Size = new System.Drawing.Size(42, 17);
+			mainDialogWindow.cb_XezzMode.TabIndex = 22;
+			mainDialogWindow.cb_XezzMode.Text = "XezzMode";
+			mainDialogWindow.cb_XezzMode.UseVisualStyleBackColor = true;
+
+			mainDialogWindow.ClientSize = new System.Drawing.Size(264, 105);
+		}
+
+		private void btn_Layout2_Click(object sender, EventArgs e)
+		{
+			mainDialogWindow.btn_left.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			mainDialogWindow.btn_left.Location = new System.Drawing.Point(87, 25);
+			mainDialogWindow.btn_left.Name = "btn_left";
+			mainDialogWindow.btn_left.Size = new System.Drawing.Size(30, 23);
+			mainDialogWindow.btn_left.TabIndex = 13;
+			mainDialogWindow.btn_left.Text = "<-";
+			mainDialogWindow.btn_left.UseVisualStyleBackColor = true;
+
+			mainDialogWindow.btn_right.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			mainDialogWindow.btn_right.Location = new System.Drawing.Point(125, 25);
+			mainDialogWindow.btn_right.Name = "btn_right";
+			mainDialogWindow.btn_right.Size = new System.Drawing.Size(30, 23);
+			mainDialogWindow.btn_right.TabIndex = 14;
+			mainDialogWindow.btn_right.Text = "->";
+			mainDialogWindow.btn_right.UseVisualStyleBackColor = true;
+
+			mainDialogWindow.btn_inv.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			mainDialogWindow.btn_inv.Location = new System.Drawing.Point(87, 52);
+			mainDialogWindow.btn_inv.Name = "btn_inv";
+			mainDialogWindow.btn_inv.Size = new System.Drawing.Size(50, 23);
+			mainDialogWindow.btn_inv.TabIndex = 15;
+			mainDialogWindow.btn_inv.Text = "inv";
+			mainDialogWindow.btn_inv.UseVisualStyleBackColor = true;
+
+			mainDialogWindow.btn_remove.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			mainDialogWindow.btn_remove.Location = new System.Drawing.Point(145, 52);
+			mainDialogWindow.btn_remove.Name = "btn_remove";
+			mainDialogWindow.btn_remove.Size = new System.Drawing.Size(75, 23);
+			mainDialogWindow.btn_remove.TabIndex = 16;
+			mainDialogWindow.btn_remove.Text = "remove";
+			mainDialogWindow.btn_remove.UseVisualStyleBackColor = true;
+
+			mainDialogWindow.lbl_requestCount.AutoSize = true;
+			mainDialogWindow.lbl_requestCount.Location = new System.Drawing.Point(71, 30);
+			mainDialogWindow.lbl_requestCount.Name = "lbl_requestCount";
+			mainDialogWindow.lbl_requestCount.Size = new System.Drawing.Size(13, 13);
+			mainDialogWindow.lbl_requestCount.TabIndex = 17;
+			mainDialogWindow.lbl_requestCount.Text = "0";
+
+			mainDialogWindow.lbl_currentNr.AutoSize = true;
+			mainDialogWindow.lbl_currentNr.Location = new System.Drawing.Point(71, 57);
+			mainDialogWindow.lbl_currentNr.Name = "lbl_currentNr";
+			mainDialogWindow.lbl_currentNr.Size = new System.Drawing.Size(13, 13);
+			mainDialogWindow.lbl_currentNr.TabIndex = 18;
+			mainDialogWindow.lbl_currentNr.Text = "0";
+
+			mainDialogWindow.lbl_currentName.AutoSize = true;
+			mainDialogWindow.lbl_currentName.Location = new System.Drawing.Point(70, 6);
+			mainDialogWindow.lbl_currentName.Name = "lbl_currentName";
+			mainDialogWindow.lbl_currentName.Size = new System.Drawing.Size(33, 13);
+			mainDialogWindow.lbl_currentName.TabIndex = 19;
+			mainDialogWindow.lbl_currentName.Text = "None";
+
+			mainDialogWindow.lbl_Pending.AutoSize = true;
+			mainDialogWindow.lbl_Pending.Location = new System.Drawing.Point(12, 30);
+			mainDialogWindow.lbl_Pending.Name = "lbl_Pending";
+			mainDialogWindow.lbl_Pending.Size = new System.Drawing.Size(17, 13);
+			mainDialogWindow.lbl_Pending.TabIndex = 20;
+			mainDialogWindow.lbl_Pending.Text = "Pending:";
+
+			mainDialogWindow.lbl_Selected.AutoSize = true;
+			mainDialogWindow.lbl_Selected.Location = new System.Drawing.Point(12, 57);
+			mainDialogWindow.lbl_Selected.Name = "lbl_Selected";
+			mainDialogWindow.lbl_Selected.Size = new System.Drawing.Size(17, 13);
+			mainDialogWindow.lbl_Selected.TabIndex = 21;
+			mainDialogWindow.lbl_Selected.Text = "Selected:";
+
+			mainDialogWindow.cb_XezzMode.AutoSize = true;
+			mainDialogWindow.cb_XezzMode.Location = new System.Drawing.Point(164, 31);
+			mainDialogWindow.cb_XezzMode.Name = "cb_XezzMode";
+			mainDialogWindow.cb_XezzMode.Size = new System.Drawing.Size(42, 17);
+			mainDialogWindow.cb_XezzMode.TabIndex = 22;
+			mainDialogWindow.cb_XezzMode.Text = "XezzMode";
+			mainDialogWindow.cb_XezzMode.UseVisualStyleBackColor = true;
+
+			mainDialogWindow.ClientSize = new System.Drawing.Size(264, 79);
+		}
+
+		private void btn_Layout3_Click(object sender, EventArgs e)
+		{
+			mainDialogWindow.btn_left.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			mainDialogWindow.btn_left.Location = new System.Drawing.Point(56, 25);
+			mainDialogWindow.btn_left.Name = "btn_left";
+			mainDialogWindow.btn_left.Size = new System.Drawing.Size(18, 23);
+			mainDialogWindow.btn_left.TabIndex = 13;
+			mainDialogWindow.btn_left.Text = "<";
+			mainDialogWindow.btn_left.UseVisualStyleBackColor = true;
+			
+			mainDialogWindow.btn_right.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			mainDialogWindow.btn_right.Location = new System.Drawing.Point(80, 25);
+			mainDialogWindow.btn_right.Name = "btn_right";
+			mainDialogWindow.btn_right.Size = new System.Drawing.Size(18, 23);
+			mainDialogWindow.btn_right.TabIndex = 14;
+			mainDialogWindow.btn_right.Text = ">";
+			mainDialogWindow.btn_right.UseVisualStyleBackColor = true;
+
+			mainDialogWindow.btn_inv.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			mainDialogWindow.btn_inv.Location = new System.Drawing.Point(56, 51);
+			mainDialogWindow.btn_inv.Name = "btn_inv";
+			mainDialogWindow.btn_inv.Size = new System.Drawing.Size(18, 23);
+			mainDialogWindow.btn_inv.TabIndex = 15;
+			mainDialogWindow.btn_inv.Text = "i";
+			mainDialogWindow.btn_inv.UseVisualStyleBackColor = true;
+
+			mainDialogWindow.btn_remove.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			mainDialogWindow.btn_remove.Location = new System.Drawing.Point(80, 51);
+			mainDialogWindow.btn_remove.Name = "btn_remove";
+			mainDialogWindow.btn_remove.Size = new System.Drawing.Size(18, 23);
+			mainDialogWindow.btn_remove.TabIndex = 16;
+			mainDialogWindow.btn_remove.Text = "r";
+			mainDialogWindow.btn_remove.UseVisualStyleBackColor = true;
+
+			mainDialogWindow.lbl_requestCount.AutoSize = true;
+			mainDialogWindow.lbl_requestCount.Location = new System.Drawing.Point(35, 30);
+			mainDialogWindow.lbl_requestCount.Name = "lbl_requestCount";
+			mainDialogWindow.lbl_requestCount.Size = new System.Drawing.Size(13, 13);
+			mainDialogWindow.lbl_requestCount.TabIndex = 17;
+			mainDialogWindow.lbl_requestCount.Text = "0";
+
+			mainDialogWindow.lbl_currentNr.AutoSize = true;
+			mainDialogWindow.lbl_currentNr.Location = new System.Drawing.Point(35, 57);
+			mainDialogWindow.lbl_currentNr.Name = "lbl_currentNr";
+			mainDialogWindow.lbl_currentNr.Size = new System.Drawing.Size(13, 13);
+			mainDialogWindow.lbl_currentNr.TabIndex = 18;
+			mainDialogWindow.lbl_currentNr.Text = "0";
+			
+			mainDialogWindow.lbl_currentName.AutoSize = true;
+			mainDialogWindow.lbl_currentName.Location = new System.Drawing.Point(65, 6);
+			mainDialogWindow.lbl_currentName.Name = "lbl_currentName";
+			mainDialogWindow.lbl_currentName.Size = new System.Drawing.Size(33, 13);
+			mainDialogWindow.lbl_currentName.TabIndex = 19;
+			mainDialogWindow.lbl_currentName.Text = "None";
+			
+			mainDialogWindow.lbl_Pending.AutoSize = true;
+			mainDialogWindow.lbl_Pending.Location = new System.Drawing.Point(12, 30);
+			mainDialogWindow.lbl_Pending.Name = "lbl_Pending";
+			mainDialogWindow.lbl_Pending.Size = new System.Drawing.Size(17, 13);
+			mainDialogWindow.lbl_Pending.TabIndex = 20;
+			mainDialogWindow.lbl_Pending.Text = "P:";
+
+			mainDialogWindow.lbl_Selected.AutoSize = true;
+			mainDialogWindow.lbl_Selected.Location = new System.Drawing.Point(12, 57);
+			mainDialogWindow.lbl_Selected.Name = "lbl_Selected";
+			mainDialogWindow.lbl_Selected.Size = new System.Drawing.Size(17, 13);
+			mainDialogWindow.lbl_Selected.TabIndex = 21;
+			mainDialogWindow.lbl_Selected.Text = "S:";
+			
+			mainDialogWindow.cb_XezzMode.AutoSize = true;
+			mainDialogWindow.cb_XezzMode.Location = new System.Drawing.Point(100, 41);
+			mainDialogWindow.cb_XezzMode.Name = "cb_XezzMode";
+			mainDialogWindow.cb_XezzMode.Size = new System.Drawing.Size(42, 17);
+			mainDialogWindow.cb_XezzMode.TabIndex = 22;
+			mainDialogWindow.cb_XezzMode.Text = "XM";
+			mainDialogWindow.cb_XezzMode.UseVisualStyleBackColor = true;
+
+			mainDialogWindow.ClientSize = new System.Drawing.Size(165, 78);
+		}
+
+		private void btn_ShowHideTitleBar_Click(object sender, EventArgs e)
+		{
+			if (mainDialogWindow.FormBorderStyle != System.Windows.Forms.FormBorderStyle.None)
+			{
+				mainDialogWindow.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			}
+			else
+			{
+				mainDialogWindow.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Sizable;
+			}
+		}
+
+		private void btn_ExitApp_Click(object sender, EventArgs e)
+		{
+			mainDialogWindow.Close();
+		}
+
+		private void btn_ResetWindowPos_Click(object sender, EventArgs e)
+		{
+			mainDialogWindow.Location = new Point(0, 0);
+		}
+
+		private void btn_SetWindowPos_Click(object sender, EventArgs e)
+		{
+			int valueX;
+			int valueY;
+
+			var text = tb_WindowPosX.Text;
+			bool isNumericX = int.TryParse(text, out valueX);
+
+			text = tb_WindowPosY.Text;
+			bool isNumericY = int.TryParse(text, out valueY);
+
+			if (isNumericX && isNumericY)
+			{
+				Point newPoint = new Point(valueX, valueY);
+				mainDialogWindow.Location = newPoint;
+			}
+		}
+	}
+}
